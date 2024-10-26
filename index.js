93d6e450471b60bcbae01fbd10ac6e089adb3ac1
KgaugeloKgatle <167708939+KgaugeloKgatle@users.noreply.github.com>
2024-10-26 16:03:14 +0200

    updated javasript

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+function refreshWeather(response){
+    let tempElement = document.querySelector("#temp");
+    let temperature = response.data.temp.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    tempElement.innerHTML=Math.round(temperature);
+
+}
+
+function searchCity(city){
+    let apiKey =  "b2a5adcct04b33178913oc335f405433";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
 
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
+
     cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElemet = document.querySelector("#search-form");
-searchFormElemet.addEventListener("submit",handleSearchSubmit)
+searchFormElemet.addEventListener("submit",handleSearchSubmit);
+
+searchCity("Tzaneen");
